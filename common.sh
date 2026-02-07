232eb556da322ecaa77f1e4f56709008a22b4c69
chakradhar seeramsetty <chakrichakradhar148@gmail.com>
2026-02-07 11:20:53 +0530

    corrected script in common.sh

--- common.sh
+++ common.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 owner=($(id -u))
 logs_dir="/var/log/RoboShop-shell" 
 log_file="$logs_dir/$0.log"
@@ -8,6 +10,7 @@ B="\e[34m"
 NC="\e[0m"
 Path=$PWD
 START_TIME=$(date +%s)
+
 mkdir -p $logs_dir
 
 check_root() {
@@ -25,10 +28,10 @@ fi
 validate(){
 
     if [ $1 -ne 0 ]; then
-        echo -e "$2 $R $(date "+%Y-%m-%d %H:%M:%S") installation failed. $NC" | tee -a $log_file
+        echo -e "$2 $R $(date "+%Y-%m-%d %H:%M:%S") ....installation failed. $NC" | tee -a $log_file
         exit 1
     else
-        echo -e "$2 $G $(date "+%Y-%m-%d %H:%M:%S") installation successful. $NC" | tee -a $log_file
+        echo -e "$2 $G $(date "+%Y-%m-%d %H:%M:%S") ....installation successful. $NC" | tee -a $log_file
     fi
 }
 
